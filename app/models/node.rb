88766ed182a6971602388b102ea86f12be247836
Andrew Edvalson <andy.edvalson@gmail.com>
2024-02-21 23:58:40 -0700

    Add nodes controller with LCA method, add ancestors method to nodes method

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,18 @@
 class Node < ApplicationRecord
+
+  # Self referential keying on parent_node
+  has_many :child_nodes, class_name: 'Node', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent_node, class_name: 'Node', optional: true, foreign_key: 'parent_id'
+
+  # Crawl up the "family tree", building a list of nodes
+  # into a list.
+  def ancestors
+    node = self
+    ancestors = []
+    while node.parent_node.present?
+      ancestors << node.parent_node
+      node = node.parent_node
+    end
+    ancestors
+  end
 end
